526085bd1b900b96be07bec63ff8897214eb3504
dodexahedron <brandon@dodecatec.com>
2015-04-04 17:55:44 -0400

    Catch NullReferenceException on undocking.
    Auto-whitespace changes.

--- EssentialsPlugin/ChatHandlers/Dock/HandleDockUndock.cs
+++ EssentialsPlugin/ChatHandlers/Dock/HandleDockUndock.cs
@@ -57,128 +57,132 @@
 			m_undocking = true;
 			try
 			{
-				String pylonName = String.Join(" ", words);
-				if (PlayerMap.Instance.GetPlayerIdsFromSteamId(userId).Count < 1)
+				String pylonName = String.Join( " ", words );
+				if ( PlayerMap.Instance.GetPlayerIdsFromSteamId( userId ).Count < 1 )
 				{
-					Communication.SendPrivateInformation(userId, string.Format("Unable to find player Id: {0}", userId));
+					Communication.SendPrivateInformation( userId, string.Format( "Unable to find player Id: {0}", userId ) );
 					return true;
 				}
 
-				long playerId = PlayerMap.Instance.GetPlayerIdsFromSteamId(userId).First();
+				long playerId = PlayerMap.Instance.GetPlayerIdsFromSteamId( userId ).First( );
 
 				Dictionary<String, List<IMyCubeBlock>> testList;
 				List<IMyCubeBlock> beaconList;
-				DockingZone.FindByName(pylonName, out testList, out beaconList, playerId);
+				DockingZone.FindByName( pylonName, out testList, out beaconList, playerId );
 
-				if (beaconList.Count == 4)
+				if ( beaconList.Count == 4 )
 				{
-					foreach (IMyCubeBlock entity in beaconList)
+					foreach ( IMyCubeBlock entity in beaconList )
 					{
-						if (!Entity.CheckOwnership(entity, playerId))
+						if ( !Entity.CheckOwnership( entity, playerId ) )
 						{
-							Communication.SendPrivateInformation(userId, string.Format("You do not have permission to use '{0}'.  You must either own all the beacons or they must be shared with faction.", pylonName));
+							Communication.SendPrivateInformation( userId, string.Format( "You do not have permission to use '{0}'.  You must either own all the beacons or they must be shared with faction.", pylonName ) );
 							return true;
 						}
 					}
 
-					IMyCubeBlock e = beaconList.First();
-					IMyCubeGrid parent = (IMyCubeGrid)e.Parent;
+					IMyCubeBlock e = beaconList.First( );
+					IMyCubeGrid parent = (IMyCubeGrid) e.Parent;
 
-					long[] beaconListIds = beaconList.Select(p => p.EntityId).ToArray();
-					long ownerId = beaconList.First().OwnerId;
-					List<DockingItem> dockingItems = Docking.Instance.Find(d => d.PlayerId == ownerId && d.TargetEntityId == parent.EntityId && d.DockingBeaconIds.Intersect(beaconListIds).Count() == 4);
-					if (dockingItems.Count < 1)
+					long[ ] beaconListIds = beaconList.Select( p => p.EntityId ).ToArray( );
+					long ownerId = beaconList.First( ).OwnerId;
+					List<DockingItem> dockingItems = Docking.Instance.Find( d => d.PlayerId == ownerId && d.TargetEntityId == parent.EntityId && d.DockingBeaconIds.Intersect( beaconListIds ).Count( ) == 4 );
+					if ( dockingItems.Count < 1 )
 					{
-						Communication.SendPrivateInformation(userId, string.Format("You have no ships docked in docking zone '{0}'.", pylonName));
+						Communication.SendPrivateInformation( userId, string.Format( "You have no ships docked in docking zone '{0}'.", pylonName ) );
 						return true;
 					}
 
-					DockingItem dockingItem = dockingItems.First();
+					DockingItem dockingItem = dockingItems.First( );
 
 					// Figure out center of docking area, and other distance information
 					double maxDistance = 99;
-					Vector3D vPos = new Vector3D(0, 0, 0);
-					foreach (IMyCubeBlock b in beaconList)
+					Vector3D vPos = new Vector3D( 0, 0, 0 );
+					foreach ( IMyCubeBlock b in beaconList )
 					{
-						Vector3D beaconPos = Entity.GetBlockEntityPosition(b);
+						Vector3D beaconPos = Entity.GetBlockEntityPosition( b );
 						vPos += beaconPos;
 					}
 
 					vPos = vPos / 4;
-					foreach (IMyCubeBlock b in beaconList)
+					foreach ( IMyCubeBlock b in beaconList )
 					{
-						Vector3D beaconPos = Entity.GetBlockEntityPosition(b);
-						maxDistance = Math.Min(maxDistance, Vector3D.Distance(vPos, beaconPos));
+						Vector3D beaconPos = Entity.GetBlockEntityPosition( b );
+						maxDistance = Math.Min( maxDistance, Vector3D.Distance( vPos, beaconPos ) );
 					}
 
-					List<IMySlimBlock> blocks = new List<IMySlimBlock>();
-					parent.GetBlocks(blocks);
-					foreach (IMySlimBlock slim_cbe in blocks)
+					List<IMySlimBlock> blocks = new List<IMySlimBlock>( );
+					parent.GetBlocks( blocks );
+					foreach ( IMySlimBlock slim_cbe in blocks )
 					{
-						if (slim_cbe is IMyCubeBlock)
+						if ( slim_cbe is IMyCubeBlock )
 						{
 							IMyCubeBlock cbe = slim_cbe.FatBlock;
-							if (cbe.GetObjectBuilderCubeBlock() is MyObjectBuilder_Cockpit)
+							if ( cbe.GetObjectBuilderCubeBlock( ) is MyObjectBuilder_Cockpit )
 							{
-								MyObjectBuilder_Cockpit c = (MyObjectBuilder_Cockpit)cbe.GetObjectBuilderCubeBlock();
-								if (c.Pilot != null)
+								MyObjectBuilder_Cockpit c = (MyObjectBuilder_Cockpit) cbe.GetObjectBuilderCubeBlock( );
+								if ( c.Pilot != null )
 								{
-									Communication.SendPrivateInformation(userId, string.Format("Carrier ship has a pilot.  The carrier should be unpiloted and fully stopped before undocking.  (Sometimes this can lag a bit.  Wait 10 seconds and try again)", pylonName));
+									Communication.SendPrivateInformation( userId,
+									                                      string.Format(
+										                                      "Carrier ship has a pilot.  The carrier should be unpiloted and fully stopped before undocking.  (Sometimes this can lag a bit.  Wait 10 seconds and try again)",
+										                                      pylonName ) );
 									return true;
 								}
 							}
 						}
 					}
 
-					String dockedShipFileName = Essentials.PluginPath + String.Format("\\Docking\\docked_{0}_{1}_{2}.sbc", ownerId, dockingItem.TargetEntityId, dockingItem.DockedEntityId);
+					String dockedShipFileName = Essentials.PluginPath + String.Format( "\\Docking\\docked_{0}_{1}_{2}.sbc", ownerId, dockingItem.TargetEntityId, dockingItem.DockedEntityId );
 
 					// Load Entity From File and add to game
-					FileInfo fileInfo = new FileInfo(dockedShipFileName);
+					FileInfo fileInfo = new FileInfo( dockedShipFileName );
 					//CubeGridEntity cubeGrid = new CubeGridEntity(fileInfo);
 
-					MyObjectBuilder_CubeGrid cubeGrid = BaseObjectManager.ReadSpaceEngineersFile<MyObjectBuilder_CubeGrid, MyObjectBuilder_CubeGridSerializer>(dockedShipFileName);
+					MyObjectBuilder_CubeGrid cubeGrid = BaseObjectManager.ReadSpaceEngineersFile<MyObjectBuilder_CubeGrid, MyObjectBuilder_CubeGridSerializer>( dockedShipFileName );
 
 					// Rotate our ship relative to our saved rotation and the new carrier rotation                
-					cubeGrid.PositionAndOrientation = new MyPositionAndOrientation(Matrix.CreateFromQuaternion(Quaternion.CreateFromRotationMatrix(parent.Physics.GetWorldMatrix().GetOrientation()) * dockingItem.SaveQuat).GetOrientation());
+					cubeGrid.PositionAndOrientation =
+						new MyPositionAndOrientation( Matrix.CreateFromQuaternion( Quaternion.CreateFromRotationMatrix( parent.Physics.GetWorldMatrix( ).GetOrientation( ) ) * dockingItem.SaveQuat ).GetOrientation( ) );
 					// Move our ship relative to the new carrier position and orientation
-					Quaternion newQuat = Quaternion.CreateFromRotationMatrix(parent.Physics.GetWorldMatrix().GetOrientation());
-					Vector3D rotatedPos = Vector3D.Transform(dockingItem.SavePos, newQuat);
+					Quaternion newQuat = Quaternion.CreateFromRotationMatrix( parent.Physics.GetWorldMatrix( ).GetOrientation( ) );
+					Vector3D rotatedPos = Vector3D.Transform( dockingItem.SavePos, newQuat );
 					//cubeGrid.Position = rotatedPos + parent.GetPosition();				
-					cubeGrid.PositionAndOrientation = new MyPositionAndOrientation(rotatedPos + parent.GetPosition(), cubeGrid.PositionAndOrientation.Value.Forward, cubeGrid.PositionAndOrientation.Value.Up);
+					cubeGrid.PositionAndOrientation = new MyPositionAndOrientation( rotatedPos + parent.GetPosition( ), cubeGrid.PositionAndOrientation.Value.Forward, cubeGrid.PositionAndOrientation.Value.Up );
 
 					// Add object to world
-					cubeGrid.EntityId = BaseEntity.GenerateEntityId();
+					cubeGrid.EntityId = BaseEntity.GenerateEntityId( );
 					cubeGrid.LinearVelocity = Vector3.Zero;
 					cubeGrid.AngularVelocity = Vector3.Zero;
 
 					bool undock = false;
-					Wrapper.GameAction(() =>
-					{
-						try
-						{
-							MyAPIGateway.Entities.CreateFromObjectBuilderAndAdd(cubeGrid);
-							List<MyObjectBuilder_EntityBase> addList = new List<MyObjectBuilder_EntityBase>();
-							addList.Add(cubeGrid);
-							MyAPIGateway.Multiplayer.SendEntitiesCreated(addList);
-							undock = true;
-						}
-						catch (Exception Ex)
-						{
-							Log.Info(string.Format("Error undocking ship: {0}", Ex.ToString()));
-							Communication.SendPrivateInformation(userId, string.Format("Unable to undock ship due to error."));
-						}
-					});
+					Wrapper.GameAction( ( ) =>
+					                    {
+						                    try
+						                    {
+							                    MyAPIGateway.Entities.CreateFromObjectBuilderAndAdd( cubeGrid );
+							                    List<MyObjectBuilder_EntityBase> addList = new List<MyObjectBuilder_EntityBase>( );
+							                    addList.Add( cubeGrid );
+							                    MyAPIGateway.Multiplayer.SendEntitiesCreated( addList );
+							                    undock = true;
+						                    }
+						                    catch ( Exception Ex )
+						                    {
+							                    Log.Info( string.Format( "Error undocking ship: {0}", Ex.ToString( ) ) );
+							                    Communication.SendPrivateInformation( userId, string.Format( "Unable to undock ship due to error." ) );
+						                    }
+					                    } );
 
-					if (!undock)
+					if ( !undock )
 						return true;
 
 					//SectorObjectManager.Instance.AddEntity(cubeGrid);
 
 					// Remove the docking file
-					File.Delete(dockedShipFileName);
-					Docking.Instance.Remove(dockingItem);
+					File.Delete( dockedShipFileName );
+					Docking.Instance.Remove( dockingItem );
 
-					Communication.SendPrivateInformation(userId, string.Format("The ship '{0}' has been undocked from docking zone '{1}'", dockingItem.DockedName, pylonName));
+					Communication.SendPrivateInformation( userId, string.Format( "The ship '{0}' has been undocked from docking zone '{1}'", dockingItem.DockedName, pylonName ) );
 
 					/*
 					// Queue for cooldown
@@ -225,14 +229,22 @@
 					PluginDocking.CooldownList.Add(cItem);
 					 */
 				}
-				else if (beaconList.Count > 4) // Too many beacons, must be 4
+				else if ( beaconList.Count > 4 ) // Too many beacons, must be 4
 				{
-					Communication.SendPrivateInformation(userId, string.Format("Too many beacons with the name or another zone with the name '{0}'.  Place only 4 beacons to create a zone or try a different zone name.", pylonName));
+					Communication.SendPrivateInformation( userId,
+					                                      string.Format( "Too many beacons with the name or another zone with the name '{0}'.  Place only 4 beacons to create a zone or try a different zone name.",
+					                                                     pylonName ) );
 				}
 				else // Can't find docking zone
 				{
-					Communication.SendPrivateInformation(userId, string.Format("Can not locate docking zone '{0}'.  There must be 4 beacons with the name '{0}' to create a docking zone.  Beacons must be fully built!", pylonName));
+					Communication.SendPrivateInformation( userId,
+					                                      string.Format( "Can not locate docking zone '{0}'.  There must be 4 beacons with the name '{0}' to create a docking zone.  Beacons must be fully built!",
+					                                                     pylonName ) );
 				}
+			}
+			catch ( NullReferenceException ex )
+			{
+				Log.Error( ex );
 			}
 			finally
 			{
