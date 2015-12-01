128e4a2670c3a8fd37ed70fd5e1cd662d7099d5a
88tom <killers1988@hotmail.com>
2015-12-01 12:05:47 +0100

    Update HandleAdminDeleteInactive.cs
    same thing of the scan inactive

--- EssentialsPlugin/ChatHandlers/AdminDelete/HandleAdminDeleteInactive.cs
+++ EssentialsPlugin/ChatHandlers/AdminDelete/HandleAdminDeleteInactive.cs
@@ -103,7 +103,7 @@
 
 				if (CubeGrids.GetAllOwners(gridBuilder).Count < 1 && removeOwnerless)
 				{
-					Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) not owned by anyone.", gridEntity.Name, entity.EntityId));
+					Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) not owned by anyone.", entity.DisplayName, entity.EntityId));
 					entitiesFound.Add(entity);
 					continue;
 				}
@@ -117,7 +117,7 @@
 					MyObjectBuilder_Checkpoint.PlayerItem checkItem = PlayerMap.Instance.GetPlayerItemFromPlayerId(player);
 					if (checkItem.IsDead || checkItem.Name == "")
 					{
-						Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) owned by dead player - ID: {2}", gridEntity.Name, entity.EntityId, player));
+						Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) owned by dead player - ID: {2}", entity.DisplayName, entity.EntityId, player));
 						entitiesFound.Add(entity);
 						continue;
 					}
@@ -127,13 +127,13 @@
 					{
 						if (removeNoLoginInformation)
 						{
-							Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) owned by a player with no login info: {2}", gridEntity.Name, entity.EntityId, checkItem.Name));
+							Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) owned by a player with no login info: {2}", entity.DisplayName, entity.EntityId, checkItem.Name));
 							entitiesFound.Add(entity);
 						}
 					}
 					else if (item.LastLogin < DateTime.Now.AddDays(days * -1))
 					{
-						Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) owned by inactive player: {2}", gridEntity.Name, entity.EntityId, PlayerMap.Instance.GetPlayerItemFromPlayerId(player).Name));
+						Communication.SendPrivateInformation(userId, string.Format("Found entity '{0}' ({1}) owned by inactive player: {2}", entity.DisplayName, entity.EntityId, PlayerMap.Instance.GetPlayerItemFromPlayerId(player).Name));
 						entitiesFound.Add(entity);
 					}
 				}
