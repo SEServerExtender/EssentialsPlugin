84c7ef2efafdc6716a66cbd5abfa77e82711f445
Brant Martin <brant@brantmartin.com>
2016-02-06 14:54:28 -0500

    This is a more efficient way of handling reserved slots

--- EssentialsPlugin/ProcessHandlers/ProcessReservedSlots.cs
+++ EssentialsPlugin/ProcessHandlers/ProcessReservedSlots.cs
@@ -17,7 +17,6 @@ namespace EssentialsPlugin.ProcessHandlers
         private static List<ulong> waitingPlayers = new List<ulong>( );
         private static List<IMyPlayer> connectedPlayers = new List<IMyPlayer>( );
         private static bool _init = false;
-        //private static int connectedCount = 0;
 
         public static void Init( )
         {
@@ -26,29 +25,28 @@ namespace EssentialsPlugin.ProcessHandlers
 
             _init = true;
             SteamSDK.SteamServerAPI.Instance.GameServer.UserGroupStatus += GameServer_UserGroupStatus;
-            
-            Essentials.Log.Info( "Reserve slot initialize" );
+
+            Essentials.Log.Info( "Reserved slots initialized" );
         }
 
         public override void OnPlayerJoined( ulong remoteUserId )
         {
-            MyAPIGateway.Players.GetPlayers( connectedPlayers );
-
-            if ( connectedPlayers.Count >= Server.Instance.Config.MaxPlayers )
-                return;
-
-            if ( reservedPlayers.Count >= PluginSettings.Instance.ReservedSlotsCount )
-                return;
-
+            //it might be better to hook into ValidateAuthTicketResponse, but doing it this way lets the game
+            //take care of denying banned players and group/friend whitelisting
+                        
             if ( PluginSettings.Instance.ReservedSlotsPlayers.Contains( remoteUserId.ToString( ) ) )
             {
-                Essentials.Log.Info( "Allocated slot to " + remoteUserId.ToString( ) );
                 reservedPlayers.Add( remoteUserId );
+                Essentials.Log.Info( "Whitelisted player connected: " + remoteUserId.ToString( ) );
+                Essentials.Log.Info( string.Format( "{0} whitelisted players connected. {1} of {2} reserved slots allocated.",
+                    reservedPlayers.Count, Math.Min( reservedPlayers.Count, PluginSettings.Instance.ReservedSlotsCount ), PluginSettings.Instance.ReservedSlotsCount ) );
+
                 return;
             }
 
             if ( PluginSettings.Instance.ReservedSlotsGroup != 0 )
             {
+                //ask Steam if the connecting player is in the whitelisted group. response is raised as an event; GameServer_UserGroupStatus
                 SteamServerAPI.Instance.GameServer.RequestGroupStatus( remoteUserId, PluginSettings.Instance.ReservedSlotsGroup );
                 waitingPlayers.Add( remoteUserId );
                 return;
@@ -59,19 +57,22 @@ namespace EssentialsPlugin.ProcessHandlers
 
         private static void DenyPlayer( ulong remoteUserId )
         {
-            Essentials.Log.Info( "deny player " + remoteUserId.ToString( ) );
+            //get the list of current players just so we can count them (this is a stupid solution)
             MyAPIGateway.Players.GetPlayers( connectedPlayers );
 
-            int publicPlayers = connectedPlayers.Count - reservedPlayers.Count;
+            int publicPlayers = connectedPlayers.Count - Math.Min( reservedPlayers.Count, PluginSettings.Instance.ReservedSlotsCount );
             int publicSlots = Server.Instance.Config.MaxPlayers - PluginSettings.Instance.ReservedSlotsCount;
-
+            
             if ( publicPlayers < publicSlots )
                 return;
 
+            //don't do anything while we're waiting for group authorization
             if ( waitingPlayers.Contains( remoteUserId ) )
                 return;
 
-            Essentials.Log.Info( "Removing user " + remoteUserId.ToString( ) );
+            //kick the player with the "Server is full" message
+            //too bad we can't send a custom message, but they're hardcoded into the client
+            Essentials.Log.Info( "Player denied: " + remoteUserId.ToString( ) );
             JoinResultMsg msg = new JoinResultMsg( );
             msg.JoinResult = JoinResult.ServerFull;
             msg.Admin = 0;
@@ -84,9 +85,10 @@ namespace EssentialsPlugin.ProcessHandlers
             {
                 if ( member || officier )
                 {
-                    if ( reservedPlayers.Count < PluginSettings.Instance.ReservedSlotsCount )
-                        reservedPlayers.Add( userId );
-                    return;
+                    reservedPlayers.Add( userId );
+                    Essentials.Log.Info( "Whitelisted player connected: " + userId.ToString( ) );
+                    Essentials.Log.Info( string.Format( "{0} whitelisted players connected. {1} of {2} reserved slots allocated.",
+                    reservedPlayers.Count, Math.Min( reservedPlayers.Count, PluginSettings.Instance.ReservedSlotsCount ), PluginSettings.Instance.ReservedSlotsCount ) );
                 }
                 else
                     DenyPlayer( userId );
@@ -95,8 +97,13 @@ namespace EssentialsPlugin.ProcessHandlers
 
         public override void OnPlayerLeft( ulong remoteUserId )
         {
+            //free up allocated slots so someone else can use it
             if ( reservedPlayers.Contains( remoteUserId ) )
+            {
+                Essentials.Log.Info( "Freed slot from: " + remoteUserId );
                 reservedPlayers.Remove( remoteUserId );
+                Essentials.Log.Info( string.Format( "{0} slots of {1} allocated.", reservedPlayers.Count, PluginSettings.Instance.ReservedSlotsCount ) );
+            }
         }
     }
 }
