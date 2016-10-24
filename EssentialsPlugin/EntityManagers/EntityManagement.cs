7ac9223f95793bd014f9ba266ed1a6e1b2a0b0bf
Brant Martin <rexxar-tc@users.noreply.github.com>
2016-10-24 16:59:42 -0400

    Cleanup obsolete conceal methods

--- EssentialsPlugin/EntityManagers/EntityManagement.cs
+++ EssentialsPlugin/EntityManagers/EntityManagement.cs
@@ -3,35 +3,24 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading;
     using EssentialsPlugin.ProcessHandlers;
     using EssentialsPlugin.Utility;
     using Sandbox.ModAPI;
     using SEModAPIInternal.API.Common;
-    using VRage.ModAPI;
-    using VRage.ObjectBuilders;
     using VRageMath;
     using Sandbox.Game.Entities;
     using Sandbox.Game.World;
     using Sandbox.Game.Entities.Blocks;
     using Sandbox.Game.Entities.Cube;
-    using Sandbox.ModAPI.Ingame;
     using SpaceEngineers.Game.ModAPI;
-    using SpaceEngineers.Game.ModAPI.Ingame;
     using VRage.Collections;
     using VRage.Game;
     using VRage.Game.Entity;
-    using VRage.Game.ModAPI;
-    using IMyBeacon = Sandbox.ModAPI.IMyBeacon;
-    using IMyMedicalRoom = SpaceEngineers.Game.ModAPI.IMyMedicalRoom;
-    using IMyProductionBlock = Sandbox.ModAPI.IMyProductionBlock;
-    using IMyRadioAntenna = Sandbox.ModAPI.IMyRadioAntenna;
 
     public class EntityManagement
     {
         private static volatile bool _checkReveal;
         private static volatile bool _checkConceal;
-        private static bool _oldInit;
         public static readonly MyConcurrentHashSet<long> RemovedGrids = new MyConcurrentHashSet<long>( );
         private static readonly List<ulong> Online = new List<ulong>( );
 
@@ -213,12 +202,12 @@
 			    if ( PluginSettings.Instance.DynamicShowMessages )
 			        Essentials.Log.Info($"{entity.EntityId} {ownerId} {ownerName}" );
 
-				pos = 2;
-                if ( entity.Physics != null )
-				{
-					entity.Physics.LinearVelocity = Vector3.Zero;
-					entity.Physics.AngularVelocity = Vector3.Zero;
-				}
+				//pos = 2;
+    //            if ( entity.Physics != null )
+				//{
+				//	entity.Physics.LinearVelocity = Vector3.Zero;
+				//	entity.Physics.AngularVelocity = Vector3.Zero;
+				//}
 
 				//pos = 3;
                 //if ( UnregisteredEntities.Contains( entity ) )
@@ -241,77 +230,9 @@
 				Essentials.Log.Error( ex, $"Failure while concealing entity {pos}." );
 			}
 		}
-
-        [Obsolete]
-        public static void CheckAndRevealEntitiesObsolete()
-        {
-
-            if (_checkReveal)
-                return;
-
-            _checkReveal = true;
-            try
-            {
-                DateTime start = DateTime.Now;
-                double br = 0f;
-                double re = 0f;
-
-                List<IMyPlayer> players = new List<IMyPlayer>();
-                HashSet<IMyEntity> entities = new HashSet<IMyEntity>();
-                MyAPIGateway.Players.GetPlayers(players);
-                Wrapper.GameAction(() =>
-                {
-                    MyAPIGateway.Entities.GetEntities(entities);
-                });
-
-                Dictionary<IMyEntity, string> entitiesToReveal = new Dictionary<IMyEntity, string>();
-                //HashSet<IMyEntity> entitiesToReveal = new HashSet<IMyEntity>();
-                foreach (IMyEntity entity in entities)
-                {
-                    if (entity.MarkedForClose)
-                        continue;
-
-                    if (!(entity is IMyCubeGrid))
-                        continue;
-
-                    if (entity.InScene)
-                        continue;
-
-                    Wrapper.GameAction( () => RevealEntityObsolete( new KeyValuePair<IMyEntity, string>( entity, "Obsolete Reveal" ) ) );
-                }
-
-                DateTime reStart = DateTime.Now;
-                re += (DateTime.Now - reStart).TotalMilliseconds;
-
-                if ((DateTime.Now - start).TotalMilliseconds > 2000 && PluginSettings.Instance.DynamicShowMessages)
-                    Essentials.Log.Info("Completed Reveal Check: {0}ms (br: {1}ms, re: {2}ms)", (DateTime.Now - start).TotalMilliseconds, br, re);
-            }
-            catch (InvalidOperationException ex)
-            {
-                if (ex.Message.StartsWith("Collection was modified"))
-                    Essentials.Log.Trace(ex);
-            }
-            catch (Exception ex)
-            {
-                Essentials.Log.Error(ex);
-            }
-            finally
-            {
-                _checkReveal = false;
-            }
-        }
-
+        
         public static void CheckAndRevealEntities( )
 		{
-            if ( !_oldInit )
-            {
-                _oldInit = true;
-                //TODO: Remove this eventually
-                //run the old reveal code in case there are any grids still concealed by the old stuff
-                CheckAndRevealEntitiesObsolete();
-                return;
-            }
-
 			if ( _checkReveal )
 				return;
 
@@ -491,91 +412,10 @@
 			return false;
 		}
         
-        [Obsolete]
-        public static void RevealEntityObsolete( KeyValuePair<IMyEntity, string> item )
-		{
-			IMyEntity entity = item.Key;
-			string reason = item.Value;
-			//Wrapper.GameAction(() =>
-			//{
-			MyObjectBuilder_CubeGrid builder = CubeGrids.SafeGetObjectBuilder( (IMyCubeGrid)entity );
-			if ( builder == null )
-				return;
-
-            if ( entity.InScene )
-                return;
-
-			IMyCubeGrid grid = (IMyCubeGrid)entity;
-			long ownerId = 0;
-			string ownerName = string.Empty;
-			if ( CubeGrids.GetBigOwners( builder ).Count > 0 )
-			{
-				ownerId = CubeGrids.GetBigOwners( builder ).First( );
-				ownerName = PlayerMap.Instance.GetPlayerItemFromPlayerId( ownerId ).Name;
-			}
-			/*
-			entity.InScene = true;
-			entity.CastShadows = true;
-			entity.Visible = true;
-			*/
-
-			builder.PersistentFlags = ( MyPersistentEntityFlags2.InScene | MyPersistentEntityFlags2.CastShadows );
-			MyAPIGateway.Entities.RemapObjectBuilder( builder );
-			//builder.EntityId = 0;
-
-			if ( RemovedGrids.Contains( entity.EntityId ) )
-			{
-				Essentials.Log.Info( "Revealing - Id: {0} DUPE FOUND Display: {1} OwnerId: {2} OwnerName: {3}  Reason: {4}",
-									 entity.EntityId,
-									 entity.DisplayName.Replace( "\r", "" ).Replace( "\n", "" ),
-									 ownerId,
-									 ownerName,
-									 reason );
-			    entity.Close( );
-			    //MyMultiplayer.ReplicateImmediatelly( MyExternalReplicable.FindByObject( entity ) );
-			}
-			else
-			{
-					IMyEntity newEntity = MyAPIGateway.Entities.CreateFromObjectBuilder( builder );
-					if ( newEntity == null )
-					{
-						Essentials.Log.Warn( "CreateFromObjectBuilder failed: {0}", builder.EntityId );
-						return;
-					}
-
-				    newEntity.Physics.Enabled = false;
-
-					RemovedGrids.Add( entity.EntityId );
-				    entity.Physics.Enabled = false;
-				    entity.Close( );
-
-                    Thread.Sleep( 50 );
-				    newEntity.Physics.Enabled = true;
-                    newEntity.InScene = true;
-                    MyAPIGateway.Entities.AddEntity( newEntity );
-                    //MyMultiplayer.ReplicateImmediatelly( MyExternalReplicable.FindByObject( newEntity ) );
-                    newEntity.Physics.LinearVelocity = Vector3.Zero;
-                    newEntity.Physics.AngularVelocity = Vector3.Zero;
-
-                    if ( PluginSettings.Instance.DynamicShowMessages )
-						Essentials.Log.Info( "Revealed - Id: {0} -> {4} Display: {1} OwnerId: {2} OwnerName: {3}  Reason: {5}",
-										 entity.EntityId,
-										 entity.DisplayName.Replace( "\r", "" ).Replace( "\n", "" ),
-										 ownerId,
-										 ownerName,
-										 newEntity.EntityId,
-										 reason );
-				
-			}
-			//});
-		}
-
 		static public void RevealAll( )
 		{
 		    _checkReveal = true;
-
-            CheckAndRevealEntitiesObsolete();
-
+            
 		    HashSet<MyEntity> entities = new HashSet<MyEntity>();
             Wrapper.GameAction( ()=>entities = MyEntities.GetEntities() );
 
