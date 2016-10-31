ba3b57dbd6f15aa559bed927febc54b3a4819da5
Brant Martin <rexxar-tc@users.noreply.github.com>
2016-10-31 12:35:56 -0400

    test

--- EssentialsPlugin/ProcessHandlers/ProcessRagdoll.cs
+++ EssentialsPlugin/ProcessHandlers/ProcessRagdoll.cs
@@ -1,44 +1,35 @@
 ﻿namespace EssentialsPlugin.ProcessHandlers
 {
-    using System;
     using System.Linq;
-    using Entities.Blocks;
-    using EssentialsPlugin;
-    using EssentialsPlugin.ChatHandlers;
-    using EssentialsPlugin.EntityManagers;
     using Sandbox.Game.Entities;
-    using Sandbox.Game.Entities.Blocks;
     using Sandbox.Game.Entities.Character;
     using Sandbox.Game.Entities.Character.Components;
     using Utility;
     using VRage.Game.Entity;
 
     public class ProcessRagdoll : ProcessHandlerBase
-	{
-		public override int GetUpdateResolution()
+    {
+        public override int GetUpdateResolution( )
         {
-                return 1000;
+            return 1000;
         }
 
-		public override void Handle()
-		{
-		    if (!PluginSettings.Instance.DisableRagdoll)
-		        return;
+        public override void Handle( )
+        {
+            if (!PluginSettings.Instance.DisableRagdoll)
+                return;
 
             MyEntity[] entities = new MyEntity[0];
-            Wrapper.GameAction( ()=> entities=MyEntities.GetEntities(  ).ToArray(  ) );
+            Wrapper.GameAction( ( ) => entities = MyEntities.GetEntities( ).ToArray( ) );
 
-		    foreach (var entity in entities)
-		    {
-		        var character = entity as MyCharacter;
+            foreach (MyEntity entity in entities)
+            {
+                MyCharacter character = entity as MyCharacter;
 
-                character?.Components.Remove<MyCharacterRagdollComponent>(  );
-		    }
+                character?.Components.Remove<MyCharacterRagdollComponent>( );
+            }
 
-			base.Handle();
-		}
-        
-
-	}
+            base.Handle( );
+        }
+    }
 }
-
